005d0b3c497a9492b27c65245d4d47a2ab5cb721
Andrew <andrewe456@gmail.com>
2018-11-15 18:50:32 -0800

    Updated gimbal driver for demonstration

--- examples/gimbalDriverNano/gimbalDriverNano.ino
+++ examples/gimbalDriverNano/gimbalDriverNano.ino
@@ -35,6 +35,11 @@ Motor * pointerToInner = &inner;
 Motor * pointerToOuter = &outer;
 MotorController motorController(pointerToInner, pointerToOuter);
 
+void balance(int loops);
+
+int BAlANCE_MODE = 12;
+int SPIN_DEMO = 13;
+
 void setup() {
   Serial.begin(9600);
   Serial.setTimeout(100);
@@ -46,131 +51,174 @@ void setup() {
   //Print function list for user selection
   Serial.println("Enter command");
   Serial.println();
+  pinMode(BAlANCE_MODE, INPUT);
+  pinMode(SPIN_DEMO, INPUT);
 }
 
 void loop() {
-  //parse command input
-  String commandArray[3]; //[move motor angle] for individual movement
-                          // or [point elevation azimuth] for pointing
-  while (!Serial.available()){
-    ;
+  
+  //take accelerometer data and react to keep gimbal level
+  if (digitalRead(BAlANCE_MODE) == HIGH) {
+    Serial.println("balancing");
+    balance(1);
+  }
+  
+  //follow demo path to show capabilities
+  else if (digitalRead(SPIN_DEMO) == HIGH){
+    Serial.println("spin demo");
+    motorController.spin(25, 1);    
   }
 
-  while(Serial.available()){
-    //String tmp = Serial.readStringUntil(' ');
-    //commandArray[i] = tmp;
-    //Serial.println(tmp);
-    //i++;
-    commandArray[0] = Serial.readStringUntil(' ');
-    commandArray[1] = Serial.readStringUntil(' ');
-    commandArray[2] = Serial.readStringUntil(' ');   
-  }
-
-  Serial.print(commandArray[0]); Serial.print(' ');
-  Serial.print(commandArray[1]); Serial.print(' ');
-  Serial.println(commandArray[2]);
-  //act based on filled command array
-  if (commandArray[0] == "point"){
-    //Serial.print("1: " + commandArray[1]); Serial.println("2: " + commandArray[2]);
-    motorController.point(commandArray[1].toDouble(), commandArray[2].toDouble());
-  }
-  else if (commandArray[0] == "move"){
-    
-    if (commandArray[1] == "outer"){
-      outer.moveMotor(commandArray[2].toDouble());
-      //Serial.println("got it");
-    }
-    else if (commandArray[1] == "inner"){
-      inner.moveMotor(commandArray[2].toDouble());
-    }
-    else {
-      Serial.println("please enter valid motor selection");
-    }
-      
-  }
-  else if (commandArray[0] == "set"){
-    if(commandArray[1] == "outer"){
-      outer.setResolution(commandArray[2].toInt());
-    }
-    else if(commandArray[1] == "inner"){
-      inner.setResolution(commandArray[2].toInt());
-    }
-    else {
-      Serial.println("please enter valid motor selection");
-    }
-  }
-  else if (commandArray[0] == "oscillate"){
-    if(commandArray[1] == "outer"){
-      outer.oscillate(MAX_ANGLE, commandArray[2].toDouble());  
-    }
-    else if(commandArray[1] == "inner"){
-      inner.oscillate(MAX_ANGLE, commandArray[2].toDouble());
-    }
-    else {
-      Serial.println("please enter valid motor selection");
-    }
-  }
-  else if (commandArray[0] == "reset"){
-    motorController.reset();
-  }
-  else if (commandArray[0] == "get"){
-    if(commandArray[1] == "outer"){
-      Serial.println(outer.getState());  
-    }
-    else if(commandArray[1] == "inner"){
-      Serial.println(inner.getState());  
-    }
-    else {
-      Serial.println("please enter valid motor selection");
-    }
-  }
-  else if (commandArray[0] == "spin") {
-    motorController.spin(commandArray[1].toDouble(), commandArray[2].toDouble());
-  }
-  else if (commandArray[0] == "osc") {
-    motorController.oscillate(commandArray[1].toDouble(), commandArray[2].toDouble());
-  }
-  else if (commandArray[0] == "path"){
-    motorController.point(20, 0); delay(500);
-    motorController.point(20, 45); delay(500);
-    motorController.point(20, 90); delay(500);
-    motorController.point(20, 135); delay(500);
-    motorController.point(20, 180); delay(500);
-    motorController.point(20, -135); delay(500);
-    motorController.point(20, -90); delay(500);
-    motorController.point(20, -45); delay(500);
-    motorController.point(20, 0); delay(500);
-    motorController.reset();
-  }
-  else if (commandArray[0] == "accel"){
-    int loops = commandArray[1].toInt();
-    Serial.print("loops: "); Serial.println(loops);
-    for(int i = 0; i < loops; i++){
-      x = analogRead(0);       // read analog input pin 0
-      y = analogRead(1);       // read analog input pin 1
-      z = analogRead(2);       // read analog input pin 20
-      x = x - OFFSET_X;
-      y = y - OFFSET_Y;
-      z = z - OFFSET_Z;
-      mag = sqrt(x*x + y*y + z*z);
-      norm_x = x/mag;
-      norm_y = y/mag;
-      norm_z = z/mag;
-      Serial.print("Vector: ");
-      Serial.print(norm_x);
-      Serial.print(" ");
-      Serial.print(norm_y);
-      Serial.print(" ");
-      Serial.println(norm_z);
-      motorController.point(norm_x, norm_y, norm_z);
-      delay(500);
-    }
-  }
+  //full functionality - -direct commanding
   else {
-    Serial.println("please enter a valid command");
-    Serial.println("[set motor resolution] to set a motor's resolution");
-    Serial.println("[move motor angle] for individual movement");
-    Serial.println("or [point elevation azimuth] for pointing");
+    Serial.println("Please input a command");
+    //parse command input
+    String commandArray[3]; //[move motor angle] for individual movement
+                            // or [point elevation azimuth] for pointing
+    while (!Serial.available()){
+      ;
+    }
+
+    while(Serial.available()){
+      //String tmp = Serial.readStringUntil(' ');
+      //commandArray[i] = tmp;
+      //Serial.println(tmp);
+      //i++;
+      commandArray[0] = Serial.readStringUntil(' ');
+      commandArray[1] = Serial.readStringUntil(' ');
+      commandArray[2] = Serial.readStringUntil(' ');   
+    }
+
+    Serial.print(commandArray[0]); Serial.print(' ');
+    Serial.print(commandArray[1]); Serial.print(' ');
+    Serial.println(commandArray[2]);
+    //act based on filled command array
+    if (commandArray[0] == "point"){
+      //Serial.print("1: " + commandArray[1]); Serial.println("2: " + commandArray[2]);
+      motorController.point(commandArray[1].toDouble(), commandArray[2].toDouble());
+    }
+    else if (commandArray[0] == "move"){
+      
+      if (commandArray[1] == "outer"){
+        outer.moveMotor(commandArray[2].toDouble());
+        //Serial.println("got it");
+      }
+      else if (commandArray[1] == "inner"){
+        inner.moveMotor(commandArray[2].toDouble());
+      }
+      else {
+        Serial.println("please enter valid motor selection");
+      }
+        
+    }
+    else if (commandArray[0] == "set"){
+      if(commandArray[1] == "outer"){
+        outer.setResolution(commandArray[2].toInt());
+      }
+      else if(commandArray[1] == "inner"){
+        inner.setResolution(commandArray[2].toInt());
+      }
+      else {
+        Serial.println("please enter valid motor selection");
+      }
+    }
+    else if (commandArray[0] == "oscillate"){
+      if(commandArray[1] == "outer"){
+        outer.oscillate(MAX_ANGLE, commandArray[2].toDouble());  
+      }
+      else if(commandArray[1] == "inner"){
+        inner.oscillate(MAX_ANGLE, commandArray[2].toDouble());
+      }
+      else {
+        Serial.println("please enter valid motor selection");
+      }
+    }
+    else if (commandArray[0] == "reset"){
+      motorController.reset();
+    }
+    else if (commandArray[0] == "get"){
+      if(commandArray[1] == "outer"){
+        Serial.println(outer.getState());  
+      }
+      else if(commandArray[1] == "inner"){
+        Serial.println(inner.getState());  
+      }
+      else {
+        Serial.println("please enter valid motor selection");
+      }
+    }
+    else if (commandArray[0] == "spin") {
+      motorController.spin(commandArray[1].toDouble(), commandArray[2].toDouble());
+    }
+    else if (commandArray[0] == "osc") {
+      motorController.oscillate(commandArray[1].toDouble(), commandArray[2].toDouble());
+    }
+    else if (commandArray[0] == "path"){
+      motorController.point(20, 0); delay(500);
+      motorController.point(20, 45); delay(500);
+      motorController.point(20, 90); delay(500);
+      motorController.point(20, 135); delay(500);
+      motorController.point(20, 180); delay(500);
+      motorController.point(20, -135); delay(500);
+      motorController.point(20, -90); delay(500);
+      motorController.point(20, -45); delay(500);
+      motorController.point(20, 0); delay(500);
+      motorController.reset();
+    }
+    else if (commandArray[0] == "balance"){
+      int loops = commandArray[1].toInt();
+      Serial.print("loops: "); Serial.println(loops);
+      for(int i = 0; i < loops; i++){
+        x = analogRead(0);       // read analog input pin 0
+        y = analogRead(1);       // read analog input pin 1
+        z = analogRead(2);       // read analog input pin 20
+        x = x - OFFSET_X;
+        y = y - OFFSET_Y;
+        z = z - OFFSET_Z;
+        mag = sqrt(x*x + y*y + z*z);
+        norm_x = x/mag;
+        norm_y = y/mag;
+        norm_z = z/mag;
+        Serial.print("Vector: ");
+        Serial.print(norm_x);
+        Serial.print(" ");
+        Serial.print(norm_y);
+        Serial.print(" ");
+        Serial.println(norm_z);
+        motorController.point(norm_x, norm_y, norm_z);
+        delay(500);
+      }
+    }
+    else {
+      Serial.println("please enter a valid command");
+      Serial.println("[set motor resolution] to set a motor's resolution");
+      Serial.println("[move motor angle] for individual movement");
+      Serial.println("or [point elevation azimuth] for pointing");
+    }
+    Serial.println();
   }
-  Serial.println();
+}
+
+void balance(int loops) {
+  Serial.print("loops: "); Serial.println(loops);
+  for(int i = 0; i < loops; i++){
+    x = analogRead(ACC_X);       // read analog input pin 0
+    y = analogRead(ACC_Y);       // read analog input pin 1
+    z = analogRead(ACC_Z);       // read analog input pin 20
+    x = x - OFFSET_X;
+    y = y - OFFSET_Y;
+    z = z - OFFSET_Z;
+    mag = sqrt(x*x + y*y + z*z);
+    norm_x = x/mag;
+    norm_y = y/mag;
+    norm_z = z/mag;
+    Serial.print("Vector: ");
+    Serial.print(norm_x);
+    Serial.print(" ");
+    Serial.print(norm_y);
+    Serial.print(" ");
+    Serial.println(norm_z);
+    motorController.point(norm_x, norm_y, norm_z);
+    delay(100);
+  }
 }
